c28370f1c902db53104d3f5424407ebb5a9e0455
V <v.donchev@serveza.net>
2025-10-25 19:21:11 +0000

    feat: add initial test file

---
+++ tests/test_initial.py
@@ -0,0 +1,10 @@
+"""Initial test file for UX module.
+
+The UX module is primarily reference code (ui-concept).
+Tests will be added as implementation progresses.
+"""
+
+
+def test_initial() -> None:
+    """Initial test to verify test infrastructure."""
+    assert True
